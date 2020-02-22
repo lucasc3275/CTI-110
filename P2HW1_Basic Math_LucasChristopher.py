77e5a79593712a4e0e48d81a7c85fcd14f684e39
lucasc3275 <61304905+lucasc3275@users.noreply.github.com>
2020-02-22 17:09:07 -0500

    Add files via upload
    Basic Math Program

---
+++ P2HW1_Basic Math_LucasChristopher.py
@@ -0,0 +1,24 @@
+#This program performs mathmatical operations on two integers supplied by the user.
+#February 18, 2020
+#CTI-110 P2HW1-Basic Math
+#Christopher Lucas
+
+#User inputs first integer.
+first_number = int(input('Enter the first number:'))
+#User inputs the second integer.
+second_number = int(input('Enter the second number:'))
+#Numbers are added.
+sum = first_number + second_number
+#Numbers are multiplied.
+product = first_number * second_number
+#The program now displays both integers as well as the sum and product.
+print ('First Number:' , format(first_number, ',d'))
+print ('Second Number:' , format(second_number, ',d'))
+print ('Sum:' , format(sum, ',d'))
+print ('Product:' , format(product, ',d'))
+
+#Instruct user to enter the first number and assign variable
+#Instruct user to enter the second number and assign variable
+#Add both numbers
+#Multiply both numbers
+#Display the numbers provided by the user as well as the sum and product of both.
